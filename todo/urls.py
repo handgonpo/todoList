b3e09568a468837a80681b6306eed39ee77879b3
Youjung <handgonpo@naver.com>
2025-04-19 23:29:26 +0900

    [feat] django (Day 4)

--- todo/urls.py
+++ todo/urls.py
@@ -1,32 +1,15 @@
 from django.urls import path, include # include 추가
-from .apis import *
-from . import views  # views 모듈을 통째로 임포트
+# from . import views  # views.todo_list() ← 접두어 붙여서 사용해야 함
+from .views import * # todo_list() ← 이름만 써도 바로 사용 가능
 
-# viewset을 위한 모듈 호출
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register("", TodoViewSet)
 
 # 127.0.0.1:8000/todo/
 urlpatterns = [
     # Django 뷰(View), or 템플릿 렌더링(template randering)
     # path("list/", views.todo_list), # 127.0.0.1:8000/todo/list/
 
-    # APIView
-    path("create/", TodoCreateAPI.as_view()),
-    path("list/", TodoListAPI.as_view()),
-    path("retrieve/<int:pk>/", TodoRetrieveAPI.as_view()),
-    path("update/<int:pk>/", TodoUpdateAPI.as_view()),
-    path("delete/<int:pk>/", TodoDeleteAPI.as_view()),
+    path("list/", todo_list), # views. 접두어 생략
 
-    # GenericAPIView 
-    path("generics/create/", TodoGenericsCreateAPI.as_view()),
-    path("generics/list/", TodoGenericsListAPI.as_view()),
-    path("generics/retrieve/<int:pk>/", TodoGenericsRetrieveAPI.as_view()),
-    path("generics/update/<int:pk>/", TodoGenericsUpdateAPI.as_view()),
-    path("generics/delete/<int:pk>/", TodoGenericsDeleteAPI.as_view()),
-
-    # Viewsets
-    path("viewsets/", include(router.urls)),
+    # create Test
+    path("create/", TodoCreateView.as_view()),
 ]
